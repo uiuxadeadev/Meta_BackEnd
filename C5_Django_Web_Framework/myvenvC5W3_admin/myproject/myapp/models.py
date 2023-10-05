d4293f40ef6d068a68dc31ec6e5dc3e79eee10a5
Tai <taiu.engineer@gmail.com>
2023-10-05 20:40:35 +0900

    C5 Django course

---
+++ C5_Django_Web_Framework/myvenvC5W3_admin/myproject/myapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    role = models.CharField(max_length=100)
+    shift = models.IntegerField()
+
+    def __str__(self):
+        # return self.first_name
+        return f"{self.last_name}, {self.first_name}"
